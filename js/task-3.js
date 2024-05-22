694cde2ef52988017a6060e6a0a4c79e48a9f0f6
maciejbr <m.broniarski@gmail.com>
2024-05-22 10:47:59 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,6 +3,18 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime = this.playTime + hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
